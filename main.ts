dba66202449d0a2761cb6a8aa5f04846852a4012
XxElectricReaperxX <71662728+XxElectricReaperxX@users.noreply.github.com>
2021-05-07 13:08:44 -0400

    debug 1

--- main.ts
+++ main.ts
@@ -1,6 +1,9 @@
 gamerbit.onEvent(GamerBitPin.P12, GamerBitEvent.Down, function () {
     radio.sendNumber(5)
 })
+gamerbit.onEvent(GamerBitPin.P16, GamerBitEvent.Down, function () {
+    radio.sendNumber(4)
+})
 gamerbit.onEvent(GamerBitPin.P1, GamerBitEvent.Down, function () {
     radio.sendNumber(1)
 })
@@ -9,8 +12,5 @@ gamerbit.onEvent(GamerBitPin.P8, GamerBitEvent.Down, function () {
 })
 gamerbit.onEvent(GamerBitPin.P2, GamerBitEvent.Down, function () {
     radio.sendNumber(2)
-})
-gamerbit.onEvent(GamerBitPin.P0, GamerBitEvent.Down, function () {
-    radio.sendNumber(4)
 })
 radio.setGroup(17)
